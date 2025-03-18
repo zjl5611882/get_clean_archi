15801dadb422bc3b0543ff53725ea202395feb4b
zhaojunlong <40941676@qq.com>
2025-03-18 09:00:35 +0800

    更新版本，修改model，去掉freed，apirest去掉警告

--- __brick__/{{name}}/data/models/{{name}}_model.dart
+++ __brick__/{{name}}/data/models/{{name}}_model.dart
@@ -1,18 +1,22 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
+// To parse this JSON data, do
+//
+//     final tabModel = tabModelFromJson(jsonString);
+
+import 'package:json_annotation/json_annotation.dart';
 import 'dart:convert';
 
-part '{{name}}_model.freezed.dart';
 part '{{name}}_model.g.dart';
-
+// 这里的model没有属性，需要重新生成
 {{name.pascalCase()}}Model {{name.camelCase()}}ModelFromJson(String str) =>
     {{name.pascalCase()}}Model.fromJson(json.decode(str));
 
 String {{name.camelCase()}}ModelToJson({{name.pascalCase()}}Model data) => json.encode(data.toJson());
 
-@freezed
-class {{name.pascalCase()}}Model with _${{name.pascalCase()}}Model {
-  const factory {{name.pascalCase()}}Model() = _{{name.pascalCase()}}Model;
+@JsonSerializable()
+class {{name.pascalCase()}}Model {
+  {{name.pascalCase()}}Model();
+  factory {{name.pascalCase()}}Model.fromJson(Map<String, dynamic> json) => _${{name.pascalCase()}}ModelFromJson(json);
 
-  factory {{name.pascalCase()}}Model.fromJson(Map<String, dynamic> json) =>
-      _${{name.pascalCase()}}ModelFromJson(json);
+  Map<String, dynamic> toJson() => _${{name.pascalCase()}}ModelToJson(this);
 }
+
--- __brick__/{{name}}/domain/rest_client/{{name}}_rest_client.dart
+++ __brick__/{{name}}/domain/rest_client/{{name}}_rest_client.dart
@@ -1,13 +1,13 @@
 import 'package:dio_http/dio_http.dart';
-import 'package:retrofit/http.dart';
+import 'package:retrofit/retrofit.dart';
 import '../../data/data.dart';
 
 part '{{name}}_rest_client.g.dart';
 
 @RestApi()
 abstract class {{name.pascalCase()}}RestClient {
-  factory {{name.pascalCase()}}RestClient() {
-    return _{{name.pascalCase()}}RestClient(Http().getDio());
+  factory {{name.pascalCase()}}RestClient({String? baseUrl, ParseErrorLogger? errorLogger}) {
+    return _{{name.pascalCase()}}RestClient(Http().getDio(),baseUrl: baseUrl, errorLogger: errorLogger);
   }
 
   @GET("url")
